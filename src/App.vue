f45f88e132e26d4031838ab28400311a5c9cca4c
Teewai57 <bobtoyoabasi@gmail.com>
2022-05-25 23:32:22 +0100

    building date

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,16 @@
         >
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northampton, UK</div>
+          <div class="location"> {{weather.name}}, {{weather.sys.country}} </div>
           <div class="date">Monday 20 January 2022</div>
         </div>
       </div>
 
       <div class="weather-box">
-        <div class="temp">9*c</div>
-        <div class="weather">Rain</div>
+        <div class="temp"> {{Math.round(weather.main.temp)}} *c</div>
+        <div class="weather"> {{weather.weather[0].main}} </div>
       </div>
     </main>
   </div>
@@ -39,7 +39,7 @@ data() {
 methods: {
   fetchWeather(e) {
     if(e.key == "Enter") {
-      fetch(`${this.api_base}weather?q=${this.query}&units=metrics&APPID=${this.api_key}`)
+      fetch(`${this.url_base}weather?q=${this.query}&units=metrics&APPID=${this.api_key}`)
       .then(res => {
         return res.json();
       }).then(this.setResults);
@@ -47,6 +47,10 @@ methods: {
   },
   setResults (results) {
     this.weather = results;
+  },
+  dateBuilder() {
+    let d = new Date();
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   }
 }
 }
