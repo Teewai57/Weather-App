7f07384d4822445efe126dcedeb6a6df7d3ac260
Teewai57 <bobtoyoabasi@gmail.com>
2022-05-25 22:39:50 +0100

    styling cold

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,24 @@ data() {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body{
+  font-family: 'Montserrat', sans-serif;
+}
+#app{
+  background-image: url('./assets/cold.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75)) ;
+}
 </style>
